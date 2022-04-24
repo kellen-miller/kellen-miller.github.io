bfd3754a31e444969802370d4a578b21c1558aad
Kellen Miller <kellen.miller12@gmail.com>
2022-04-24 00:24:59 -0400

    added light/dark switch
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,19 +1,41 @@
-function toggleTheme() {
-    console.log("i got clicked!!!")
-    let theme = sessionStorage.getItem('theme');
-    let lightTheme = document.getElementById('light_theme');
-    let darkTheme = document.getElementById('dark_theme');
-    let icon = document.getElementById("light_dark_icon");
-    if (theme === 'dark') {
-        lightTheme.setAttribute('rel', 'stylesheet alternate');
-        darkTheme.setAttribute('rel', 'stylesheet');
-        icon.setAttribute("src", "/assets/images/light.png")
-        sessionStorage.setItem('theme', 'dark');
-    } else {
-        darkTheme.setAttribute('rel', 'stylesheet alternate');
-        lightTheme.setAttribute('rel', 'stylesheet');
-        icon.setAttribute("src", "/assets/images/dark.png")
-        sessionStorage.setItem('theme', 'light');
-    }
-    return false;
-}
+document
+    .getElementById("light_dark_icon")
+    .addEventListener("click",
+        function() {
+            console.log("i got clicked!!!")
+            let theme = sessionStorage.getItem('theme');
+            let lightTheme = document.getElementById('light_theme');
+            let darkTheme = document.getElementById('dark_theme');
+            let icon = document.getElementById("light_dark_icon");
+            if (theme === 'dark') {
+                lightTheme.setAttribute('rel', 'stylesheet alternate');
+                darkTheme.setAttribute('rel', 'stylesheet');
+                icon.setAttribute("src", "/assets/images/light.png")
+                sessionStorage.setItem('theme', 'dark');
+            } else {
+                darkTheme.setAttribute('rel', 'stylesheet alternate');
+                lightTheme.setAttribute('rel', 'stylesheet');
+                icon.setAttribute("src", "/assets/images/dark.png")
+                sessionStorage.setItem('theme', 'light');
+            }
+        }, false);
+
+// function toggleTheme() {
+//     console.log("i got clicked!!!")
+//     let theme = sessionStorage.getItem('theme');
+//     let lightTheme = document.getElementById('light_theme');
+//     let darkTheme = document.getElementById('dark_theme');
+//     let icon = document.getElementById("light_dark_icon");
+//     if (theme === 'dark') {
+//         lightTheme.setAttribute('rel', 'stylesheet alternate');
+//         darkTheme.setAttribute('rel', 'stylesheet');
+//         icon.setAttribute("src", "/assets/images/light.png")
+//         sessionStorage.setItem('theme', 'dark');
+//     } else {
+//         darkTheme.setAttribute('rel', 'stylesheet alternate');
+//         lightTheme.setAttribute('rel', 'stylesheet');
+//         icon.setAttribute("src", "/assets/images/dark.png")
+//         sessionStorage.setItem('theme', 'light');
+//     }
+//     return false;
+// }

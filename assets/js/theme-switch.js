dd34d6aac8bf36ea813ddaa8a7a7994179bdee2e
Kellen Miller <kellen.miller12@gmail.com>
2022-04-24 00:03:20 -0400

    added light/dark switch
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

---
+++ assets/js/theme-switch.js
@@ -0,0 +1,21 @@
+function setInitialTheme() {
+    sessionStorage.setItem('theme', 'light');
+}
+
+function toggleTheme() {
+    let theme = sessionStorage.getItem('theme');
+    let lightTheme = document.getElementById('light_theme');
+    let darkTheme = document.getElementById('dark_theme');
+    let icon = document.getElementById("light_dark_icon");
+    if (theme === "dark") {
+        lightTheme.setAttribute('rel', 'stylesheet alternate');
+        darkTheme.setAttribute('rel', 'stylesheet');
+        icon.setAttribute("src", "/assets/images/dark.png")
+        sessionStorage.setItem('theme', 'dark');
+    } else {
+        darkTheme.setAttribute('rel', 'stylesheet alternate');
+        lightTheme.setAttribute('rel', 'stylesheet');
+        icon.setAttribute("src", "/assets/images/light.png")
+        sessionStorage.setItem('theme', 'light');
+    }
+}
--- assets/js/toggle-theme.js
+++
@@ -1,6 +0,0 @@
-function toggleTheme() {
-    const theme = document.getElementsByTagName('link')[0];
-    theme.getAttribute('href') === "{{ '/assets/css/main.css' | relative_url }}"
-        ? theme.setAttribute('href', "{{ '/assets/css/dark.css' | relative_url }}")
-        : theme.setAttribute('href', "{{ '/assets/css/main.css' | relative_url }}")
-}
